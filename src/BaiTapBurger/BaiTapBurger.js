8be72e5640e83157887f4d482aaf045593abcbaf
quynhcfc <vanquynh.cfc@gmail.com>
2022-05-20 14:34:54 +0700

    Update file BurgerCSS to css folder

--- src/BaiTapBurger/BaiTapBurger.js
+++ src/BaiTapBurger/BaiTapBurger.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import BugerComponent from "./BurgerComponent";
 import MenuComponent from "./MenuComponent";
-import "./BurgerCSS.css";
+import "./CSS/BurgerCSS.css";
 
 export default class BaiTapBurger extends Component {
   render() {
     return (
       <div>
         <div className="container">
-          <h1 className="my-4">Cửa hàng Burger Cybersoft</h1>
+          <h1 className="my-4">Cửa hàng Burger</h1>
           <div className="row">
             <BugerComponent />
             <MenuComponent />
